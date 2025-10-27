bc3083b65365e799ca673ce5905d0bbfbaa74570
Suryabhan17 <suryabhanuday99@gmail.com>
2025-10-27 14:40:50 +0530

    cherry

--- add.java
+++ add.java
@@ -2,5 +2,7 @@
 public class add{
   public static void main(String[] args) {
     System.out.println("Suryabhan122451");
+    System.out.println("Suryabhan1224");
+    System.out.println(124);
   }
 }
